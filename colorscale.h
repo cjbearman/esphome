481658a0314c134873639e37aa412c0bbb416700
Chris Bearman <chris@chrisjb.com>
2024-10-20 17:13:44 -0500

    Add more libs

---
+++ colorscale.h
@@ -0,0 +1,22 @@
+static uint32_t COL_DARK_BLUE = 0x0000FF;
+static uint32_t COL_LIGHT_BLUE = 0x8888FF;
+static uint32_t COL_GREEN = 0x00FF00;
+static uint32_t COL_YELLOW = 0xFFFF00;
+static uint32_t COL_RED = 0xFF0000;
+
+uint32_t indoorColor(float temp) {
+        return temp <= 68 ? COL_DARK_BLUE :
+                temp <= 70 ? COL_LIGHT_BLUE :
+                temp <= 73 ? COL_GREEN :
+                temp <= 76 ? COL_YELLOW :
+                COL_RED;
+}
+
+uint32_t outdoorColor(float temp) {
+        return temp <= 32 ? COL_DARK_BLUE :
+                temp <= 50 ? COL_LIGHT_BLUE :
+                temp <= 75 ? COL_GREEN :
+                temp <= 90 ? COL_YELLOW :
+                COL_RED;
+}
+
